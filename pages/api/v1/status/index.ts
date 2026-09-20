5b53fa773ca351410a9cb0b083a00e8a606bdea2
lucas louvem <lucaslouvem2@gmail.com>
2026-09-20 10:55:18 -0300

    feat(API): implementa a resposta de status da API

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -2,16 +2,33 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { query } from "infra/database";
 
 type StatusResponse = {
-  status: string;
+  updated_at: string;
+  db_version: string;
+  usage_connections: number;
+  max_connections: number;
 };
 
 export default async function status(
   req: NextApiRequest,
   res: NextApiResponse<StatusResponse>
 ) {
-  const test = await query("SELECT 1 + 1;");
-  console.log(test.rows);
+  const updatedAt = new Date().toISOString();
+
+  const maxConnections = await query("SHOW max_connections");
+  const maxUsageConnections = maxConnections.rows[0].max_connections;
+
+  const usageConnections = await query(
+    "SELECT count(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = 'local_db'"
+  );
+  const usageConnection = usageConnections.rows[0].opened_connections;
+
+  const dbVersion = await query("SHOW server_version");
+  const serverVersion = dbVersion.rows[0].server_version;
+
   return res.status(200).send({
-    status: "ok",
+    updated_at: updatedAt,
+    usage_connections: usageConnection,
+    max_connections: parseInt(maxUsageConnections),
+    db_version: serverVersion,
   });
 }
--- test/integration/api/v1/status/get.test.ts
+++ test/integration/api/v1/status/get.test.ts
@@ -1,10 +1,10 @@
 const url = "http:127.0.0.1:3000";
 
 interface StatusResponse {
-  name: string;
-
-  como_resolver: string;
-  status_code: number;
+  updated_at: Date;
+  db_version: string;
+  usage_connections: number;
+  max_connections: number;
 }
 
 it("Fetch para Status page deve retornar 200", async () => {
@@ -14,8 +14,13 @@ it("Fetch para Status page deve retornar 200", async () => {
 
   const responseBody = (await response.json()) as StatusResponse;
 
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
   expect(responseBody).toEqual({
-    status: "ok",
+    updated_at: parsedUpdatedAt,
+    db_version: "16.15",
+    usage_connections: 1,
+    max_connections: 100,
   });
 });
 
